0a2e94731cf2f216546bd015d4b3d37cee813b8a
vrana <vrana>
2025-05-25 13:38:21 +0200

    s

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="w-full bg-[#141414] text-white p-4 border-b-1 border-b-neutral-600">
       <ul>
         <li>
-          <Link href="/user">User Management</Link>
+          <Link href="/user">Users Management</Link>
         </li>
       </ul>
     </nav>
