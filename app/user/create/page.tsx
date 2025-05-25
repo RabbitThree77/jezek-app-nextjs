edbda7398727197e9539f3f88da2aec898712328
vrana <vrana>
2025-05-25 14:00:34 +0200

    typo correction

--- app/user/create/page.tsx
+++ app/user/create/page.tsx
@@ -6,7 +6,7 @@ export default async function Page() {
     return (
         <form action={createUser}>
             <label>
-                Eneter a name for the person
+                Enter a name for the person
                 <input type="text" name="name" id="name" />
             </label>
             <button type="submit">Submit</button>
