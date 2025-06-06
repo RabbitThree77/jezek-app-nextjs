c0b4a19f30684b46b027d512f7027149fff54699
RabbitThree77 <zjck33@gmail.com>
2025-06-06 20:04:01 +0200

    make select multiple more obvious

--- app/invoice/selection/page.tsx
+++ app/invoice/selection/page.tsx
@@ -5,37 +5,37 @@ import { getAllUsers } from "@/app/lib/data";
 import "@/app/globals.css";
 
 export default async function Page() {
-    await checkAuth();
-    const users = await getAllUsers();
-    return (
-        <div className="flex w-full h-screen justify-center items-center text-center">
-            <form action={getPayingPerson}>
-                <select
-                    name="users"
-                    id="users"
-                    multiple
-                    className="w-80 custom-scrollbar"
-                >
-                    <option value="" disabled>
-                        Select the atendees
-                    </option>
-                    {users.map((user) => (
-                        <option
-                            value={user.name.toString()}
-                            key={user.id.toString()}
-                        >
-                            {user.name}
-                        </option>
-                    ))}
-                </select>
-                <br />
-                <button
-                    type="submit"
-                    className="pl-4 pr-4 pt-2 pb-2 bg-white text-black rounded"
-                >
-                    Submit
-                </button>
-            </form>
-        </div>
-    );
+	await checkAuth();
+	const users = await getAllUsers();
+	return (
+		<div className="flex w-full h-screen justify-center items-center text-center">
+			<form action={getPayingPerson}>
+				<select
+					name="users"
+					id="users"
+					multiple
+					className="w-80 custom-scrollbar border-1 text-center p-2 rounded-2xl"
+				>
+					<option value="" disabled>
+						Select the atendees
+					</option>
+					{users.map((user) => (
+						<option
+							value={user.name.toString()}
+							key={user.id.toString()}
+						>
+							{user.name}
+						</option>
+					))}
+				</select>
+				<br />
+				<button
+					type="submit"
+					className="pl-4 pr-4 pt-2 pb-2 bg-white text-black rounded"
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
